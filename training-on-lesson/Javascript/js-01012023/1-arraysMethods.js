5765325a33e35c89fbcc6dc0649985ddc7c705ec
caliskanadem <ad3m.calikan@gmail.com>
2023-01-04 17:11:14 +0000

    add

---
+++ training-on-lesson/Javascript/js-01012023/1-arraysMethods.js
@@ -0,0 +1,29 @@
+//? ===============================================
+//?           DIZI ERISIM METOTLARI
+//? ===============================================
+
+console.log("**** ACCESS ****");
+const numbers = [3, 5, 2, "4", "four", 3, "4", "bes", "1"];
+
+//* includes()
+//*-----------------------------------------------------------
+console.log(number.includes(4));
+
+//* indexOf(), lastIndexOf();
+//*-----------------------------------------------------------
+//* ilk eslesen indeksi dondurur. Bulunumazsa -1 dondurur.
+
+//* join()
+//*-----------------------------------------------------------
+//? join, dizinin elamanlari birlestirip string'e cevirir.
+
+//* toString()
+//*-----------------------------------------------------------
+//? toString fonksiyonu sadece dizinin elemanlarinin aralarina
+//? (virgul) koyarak birlestirir ve string yapar.
+
+//* slice(start, end) -- end exclusive
+//*-----------------------------------------------------------
+
+//* concat()
+//*-----------------------------------------------------------
