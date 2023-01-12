63bd1e8d91cd1a1f62d3cb5b75c01c2579ae968e
caliskanadem <ad3m.calikan@gmail.com>
2023-01-12 17:37:13 +0000

    add

--- training-on-lesson/JS-DOM/11012023/uygulama/1-selectors.js
+++ training-on-lesson/JS-DOM/11012023/uygulama/1-selectors.js
@@ -66,6 +66,36 @@ itemListSpread.map((x) => console.log((x.style.color = "red")));
 //*              QUERYSELECTOR()
 //* ========================================
 
-//* ========================================
+const myLi = document.querySelector("li");
+console.log(myLi);
+myLi.style.color = "purple";
+
+const addItem = document.querySelector(".add-item");
+console.log(addItem);
+addItem.style.border = "3px solid grey";
+
+const addBtn = document.querySelector("#btn");
+console.log(addBtn);
+addBtn.style.cursor = "pointer";
+
+const itemH2 = document.querySelector(".item-list h2");
+
+console.log(itemH2);
+
+// const addBtnType = document.querySelector("input['type=button']");
+// console.log(addBtnType.value);
+// //* ========================================
 //*              QUERYSELECTORALL()
 //* ========================================
+
+const lists = document.querySelectorAll("ul li");
+console.log(lists);
+
+lists.forEach((li) => console.log(li.innerText));
+
+console.log(
+  document.querySelectorAll("section.item-list ul li:nth-child(4)")[0].innerText
+);
+
+document.querySelector("section.item-list ul li:nth-child(4)").innerHTML =
+  "<p>Paragraf</p>";
